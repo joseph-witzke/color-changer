452a15548bb21348298ec01ec971177e69ffbf58
Joe Witzke <joewitzke@Joes-MacBook-Pro.local>
2021-12-30 11:12:45 -0500

    completes input component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='App'>
       <Square colorValue={colorValue} />
-      <Input />
+      <Input colorValue={colorValue} setColorValue={setColorValue} />
     </div>
   );
 }
